f9ca823600b87dbc038ed9270c7810542b19ab0b
Anshika Chauhan <anshikachauhan212002@gmail.com>
2025-09-09 21:35:39 +0530

    added basic project folders

---
+++ backend/src/test/java/com/example/myjourney/MyjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
